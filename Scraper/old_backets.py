21db5b3f7f9b81da5fa1b649526292bcd800cc75
Nick Purcell <npurcell@amfam.com>
2021-03-16 09:23:55 -0500

    cleanup and eval 2021 winners for logistic regression and xgboost

---
+++ Scraper/old_backets.py
@@ -0,0 +1,92 @@
+# 2019 Bracket below
+# school_names_south = [
+#     # south region
+#     ('Virginia', 1),('Gardner-Webb', 16),
+#     ('Ole Miss', 8), ('Oklahoma', 9),
+#     ('Wisconsin',5), ('Oregon', 12),
+#     ('Kansas State',4), ('UC-Irvine', 13),
+#     ('Villanova', 6), ("Saint Mary's", 11),
+#     ('Purdue',3), ('Old Dominion',14),
+#     ('Cincinnati',7),('Iowa',10),
+#     ('Tennessee',2), ('Colgate',15)
+#     ]
+# school_names_west = [
+#     # west region
+#     ('Gonzaga', 1),('Prairie View',16), #or 'Texas Southern',
+#     ('Syracuse', 8),('Baylor', 9),
+#     ('Marquette',5), ('Murray State', 12),
+#     ('Florida State',4), ('Vermont',13),
+#     ('Buffalo',6),('Arizona State',11), # or
+#     ('Texas Tech', 3),('Northern Kentucky', 14),
+#     ('Nevada',7),('Florida',10),
+#     ('Michigan',2),('Montana',15)
+#     ]
+# school_names_east = [
+#     # east region
+#     ('Duke',1),('North Carolina Central',16), # or 'Radford',
+#     ('VCU',8), ('UCF',9),
+#     ('Mississippi State',5), ('Liberty',12),
+#     ('Virginia Tech',4), ('Saint Louis',13),
+#     ('Maryland',6), ('Temple',11), # or 'UCLA',
+#     ('LSU',3), ('Yale',14),
+#     ('Louisville',7), ('Minnesota',10),
+#     ('Michigan State', 2), ('Bradley',15)
+#     ]
+# school_names_midwest = [
+#     # mid-west region
+#     ('UNC', 1),('Iona', 16),
+#     ('Utah State', 8), ('Washington', 9),
+#     ('Auburn',5), ('New Mexico State', 12),
+#     ('Kansas',4), ('Northeastern', 13),
+#     ('Iowa State', 6), ('Ohio State', 11),
+#     ('Houston',3), ('Georgia State',14),
+#     ('Wofford',7),('Seton Hall',10),
+#     ('Kentucky',2), ('Abilene Christian',15)
+#     ]
+'''
+2018 Bracket
+school_names_south = [
+    # south region
+    ('Virginia', 1),('Maryland-Baltimore County', 16),
+    ('Creighton', 8), ('Kansas State', 9),
+    ('Kentucky',5), ('Davidson', 12),
+    ('Arizona',4), ('Buffalo', 13),
+    ('Miami (FL)', 6), ('Loyola (IL)', 11),
+    ('Tennessee',3), ('Wright State',14),
+    ('Nevada',7),('Texas',10),
+    ('Cincinnati',2), ('Georgia State',15)
+    ]
+school_names_west = [
+    # west region
+    ('Xavier', 1),('North Carolina Central',16), #or 'Texas Southern',
+    ('Missouri', 8),('Florida State', 9),
+    ('Ohio State',5), ('South Dakota State', 12),
+    ('Gonzaga',4), ('North Carolina-Greensboro',13),
+    ('Houston',6),('San Diego State',11),
+    ('Michigan', 3),('Montana', 14),
+    ('Texas A&M',7),('Providence',10),
+    ('North Carolina',2),('Lipscomb',15)
+    ]
+school_names_east = [
+    # east region
+    ('Villanova',1),('Long Island University',16), # or 'Radford',
+    ('Virginia Tech',8), ('Alabama',9),
+    ('West Virginia',5), ('Murray State',12),
+    ('Wichita State',4), ('Marshall',13),
+    ('Florida',6), ('St. Bonaventure',11), # or 'UCLA',
+    ('Texas Tech',3), ('Stephen F. Austin',14),
+    ('Arkansas',7), ('Butler',10),
+    ('Purdue', 2), ('Cal State Fullerton',15)
+    ]
+school_names_midwest = [
+    # mid-west region
+    ('Kansas', 1), ('Pennsylvania',16),
+    ('Seton Hall', 8), ('North Carolina State',9),
+    ('Clemson', 5), ('New Mexico State',12),
+    ('Auburn',4), ('College of Charleston',13),
+    ('Texas Christian',6), ('Arizona State',11), # or 'Syracuse',
+    ('Michigan State',3), ('Bucknell',14),
+    ('Rhode Island',7), ('Oklahoma',10),
+    ('Duke', 2), ('Iona' ,15) 
+    ]
+'''
